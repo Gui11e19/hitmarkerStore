9e30011cd68631bc1b105444665a7b7be6347f59
Guille19 <guille19@debian>
2019-11-26 12:10:50 -0600

    Add: rutas

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('home');
 });
-router.get('/home', function(req, res) {
+
+router.get('/home', function(req, res, next) {
   res.render('home');
 });
 
@@ -13,5 +14,44 @@ router.get('/error', function(req, res) {
   res.render('error');
 });
 
+router.get('/articulos', function(req, res) {
+  res.render('articulos');
+});
+
+router.get('/about', function(req, res) {
+  res.render('about');
+});
+
+router.get('/carro', function(req, res) {
+  res.render('carro');
+});
+
+router.get('/help', function(req, res) {
+  res.render('help');
+});
+
+router.get('/index', function(req, res) {
+  res.render('index');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.get('/perfil', function(req, res) {
+  res.render('perfil');
+});
+
+router.get('/producto', function(req, res) {
+  res.render('producto');
+});
+
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+router.get('/reset_pass', function(req, res) {
+  res.render('reset_pass');
+});
 
 module.exports = router;
