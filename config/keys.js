66e97021e4769d68afe46206c5e021a1d67ac192
Guille19 <guille19@debian>
2019-11-25 22:44:22 -0600

    Agregando mas dependencias

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+
+let database = "onlineStore";
+let port = "27017";
+let host = "localhost";
+let uri = `mongodb://${host}:${port}/${database}`
+
+module.exports = {
+    mongodb:{
+        URI:'
+    }
+}
