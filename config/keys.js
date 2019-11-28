754d43ed94f155d62ad027c8711c78f492b634fd
Ruben-Aristides <00181317@uca.edu.sv>
2019-11-27 23:31:53 -0600

    COnexion de base de datos de mongo atlas, areglado

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 /*Contiene los string de conexion a la base de datos */
 module.exports = {
     mongodb:{
-        URI:'mongodb://localhost:27017/admTareas'
+        //URI:'mongodb://localhost:27017/admTareas'
+        //URI:'mongodb+srv://admin2:1234@hitmarker-mdyyg.mongodb.net/admTareas'
+        URI:"mongodb://admin2:1234@hitmarker-shard-00-00-mdyyg.mongodb.net:27017,hitmarker-shard-00-01-mdyyg.mongodb.net:27017,hitmarker-shard-00-02-mdyyg.mongodb.net:27017/test?ssl=true&replicaSet=hitmarker-shard-0&authSource=admin&retryWrites=true&w=majority"
     }
 }
